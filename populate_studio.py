7229712bedd7a66e7e4969a0d106e6494666ec88
Rodrigo Souto <rodrigo@colivre.coop.br>
2010-03-31 17:51:09 -0300

    Starting...

---
+++ populate_studio.py
@@ -0,0 +1,103 @@
+from random import randint
+from itertools import product
+
+def combinator(first_names, last_names, separator = " ", prefix="", sufix=""):
+    return map("".join,product([prefix], first_names, [separator], last_names, [sufix]))
+
+def generate_numbers(number, start=1, type="int"):
+    numbers = []
+    if type == "float":
+        start = float(start)
+    for i in range(number):
+        numbers.append(start)
+        start += 1
+    return numbers
+
+def generate_phones(number):
+    phones = []
+    for i in range(number):
+        phone.append("".join(str(randint(0,9)) for x in range(8)))
+    return phones
+
+def generate_people():
+    first_names = ["Joao", "Maria", "Dedeu", "Diguliu", "Michael", "Daniela", "Kratus", "Chuck", "Johnny", "Ze", "Conf", "Joca"]
+    last_names = ["Silva", "Oliveira", "Alcantra", "Souto", "Jackson", "Norris", "Santos", "Neto", "Braga", "Correa", "Depp", "Jordan" ]
+    
+    people_names = combinator(first_names, last_names)
+    people_emails = combinator(map(str.lower,first_names), map(str.lower,last_names), "@", "", ".com")
+    people_addresses = combinator(first_names, last_names, " - ", "Street ")
+    people_cpfs = generate_numbers(len(people_names),345746345)
+    people_cpfs = map(str, people_cpfs)
+
+    return [people_cpfs, people_names, people_addresses, people_emails]
+
+def generate_bands():
+    first_names = ["The", "Arctic", "Pearl", "Led", "Avenged"]
+    last_names = ["Zeppelin", "Monkeys", "Sevenfold", "Jam", "Beatles"]
+    band_names = combinator(first_names, last_names)
+
+    logins = combinator(map(str.lower,first_names), map(str.lower,last_names),"_")
+    passwords = generate_numbers(len(logins))
+    passwords = map(str, passwords)
+
+    first_style_names = ["Hard", "Black", "Emo", "Heavy", "Folk"]
+    last_style_names = ["Metal", "Core", "Punk", "Grunge", "Rock"]
+    style_names = combinator(first_style_names, last_style_names)
+
+    return [logins, band_names, style_names, ["www.all-bands.com"]*len(logins), passwords]
+
+def generate_equipments():
+    description = ["Semi-Acoustic", "Eletric", "Acoustic", "Tuned", "Golden"]
+    models = ["Les Paul", "Stratocaster", "Flying V", "SG", "Telecaster"]
+    types = ["Guitar", "Bass", "Drums", "Microphone", "Amplificator"]
+
+    equipment_types = combinator(types, models)
+    equipment_ids = generate_numbers(len(equipment_types))
+    internal_prices = generate_numbers(len(equipment_types),20,"float")
+    external_prices = generate_numbers(len(equipment_types),50,"float")
+
+    return [equipment_ids, ["Model"]*len(equipment_types), equipment_types, internal_prices, external_prices]
+
+def generate_services():
+    first_names = ["Record", "Sell", "Rent", "Practice", "Make"]
+    last_names = ["Albuns", "Instruments", "Equipments", "Rooms", "Dvds"]
+
+    service_names = combinator(first_names, last_names)
+    service_ids = generate_numbers(len(service_names))
+    prices = generate_numbers(len(service_names),30,"float")
+
+    return [service_ids, service_names, prices, ["Description"]*len(service_names)]
+
+def generate_insert_sql(attributes, data, table_name):
+    colums = ""
+    sql_code = ""
+    data = zip(*data)
+    for attribute in attributes:
+        colums += ',' + attribute
+    colums = colums[1:]
+    for entity in data:
+        values = ""
+        for att in entity:
+            if type(att).__name__ != "int" and type(att).__name__ != "float":
+                att = "\'{0}\'".format(att)
+            values += ',' + str(att)
+        values = values[1:]
+        sql_code += "INSERT INTO %(table_name)s (%(colums)s)\nVALUES (%(values)s);" % \
+                {   'table_name'    : table_name,\
+                    'colums'        : colums,\
+                    'values'        : values}
+        sql_code += "\n\n"
+    return sql_code
+
+
+person_attributes = ["Cpf", "Name", "Address", "Email"]
+band_attributes = ["Login", "Name", "Style", "HomePage", "Pass"]
+equipment_attributes = ["Equipment_ID", "Model", "Equipment_Type", "InternalPrice", "ExternalPrice"]
+service_attributes = ["Service_ID", "Name", "Price", "Description"]
+sql_code = generate_insert_sql(person_attributes, generate_people(), "Person")
+sql_code += generate_insert_sql(band_attributes, generate_bands(), "Band")
+sql_code += generate_insert_sql(equipment_attributes, generate_equipments(), "Equipment")
+sql_code += generate_insert_sql(service_attributes, generate_services(), "Service")
+file = open("sql_code","w")
+file.write(sql_code)
+file.close()
