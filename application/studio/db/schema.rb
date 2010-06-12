6fcee3f58697b955f4967debe5cab42d10db9987
Rodrigo Souto <rodrigo@colivre.coop.br>
2010-06-12 00:38:04 -0300

    Pseudo-Folder on show views ^^

--- application/studio/db/schema.rb
+++ application/studio/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100610035917) do
+ActiveRecord::Schema.define(:version => 20100612021518) do
 
   create_table "agendas", :force => true do |t|
     t.datetime "start",       :null => false
